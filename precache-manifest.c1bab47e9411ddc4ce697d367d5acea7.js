0e47338e0209b37ef5611b523a2f0d91f17b0bf9
Ntn172 <nitindhiman1712@gmail.com>
2020-03-28 13:58:12 +0530

    Updates

---
+++ precache-manifest.c1bab47e9411ddc4ce697d367d5acea7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd22e4d3d93f4647602f4b9d56d24c0",
+    "url": "/react-todolist/index.html"
+  },
+  {
+    "revision": "94765777122816cb973b",
+    "url": "/react-todolist/static/css/main.caee1da9.chunk.css"
+  },
+  {
+    "revision": "b802389ed20fb867c5a6",
+    "url": "/react-todolist/static/js/2.cd2b1318.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todolist/static/js/2.cd2b1318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94765777122816cb973b",
+    "url": "/react-todolist/static/js/main.ec45bee4.chunk.js"
+  },
+  {
+    "revision": "e153f21d37fad94fa161",
+    "url": "/react-todolist/static/js/runtime-main.02ef97f9.js"
+  }
+]);
